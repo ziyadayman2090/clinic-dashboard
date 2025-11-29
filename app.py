dbf75f0bf4c94fc2219fc53d3e1278ebd437b1b4
ziyadayman2090 <zyadksa331@gmail.com>
2025-11-29 04:53:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -326,7 +326,7 @@ st.markdown("---")
 # ======================
 view = st.radio(
     "View",
-    ["Overview", "Platforms", "Time analysis"],
+    ["📈 Overview", "📱 Platforms", "⏱ Time analysis"],
     horizontal=True,
 )
 
